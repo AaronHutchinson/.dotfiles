f0edb513e84aadd22c727aed0f5a7d8877deb3a2
Aaron Hutchinson <hutchinsona2018@gmail.com>
2025-05-09 23:50:46 -0700

    Initial commit with README and script for creating symbolic links.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dotfiles
+
+This repository contains my dotfiles for easily setting up my environment.
+
+# Usage
+
+Run `symlinks.sh` to create symbolic links in your home directory to the files in this repository.
+Alternatively, create the symlinks yourself, or copy the desired files into your home directory.
---
+++ symlinks.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+HOME_DIR=$( realpath ~ )
+
+LN=$( which ln )
+if [ ! $LD -n ]; then
+    echo Could not locate ln. Exiting.
+    exit
+fi
+
+declare -a dotfiles=(
+    "placeholder"
+)
+
+for dotfile in "${dotfiles[@]}"
+do
+    [ ! -f "$HOME_DIR/$dotfile" ]
+    target_exists=$?
+    
+    [ ! -f "$SCRIPT_DIR/$dotfile" ]
+    source_exists=$?
+
+    if [ $source_exists -eq 0 ]; then
+	echo "Error: source file \"$dotfile\" not found in repository."
+	continue
+    fi
+
+    result=0
+    while [ "$result" != "y" ] && [ "$result" != "n" ] && [ "$result" != "q" ];
+    do
+	if [ $target_exists -eq 0 ]; then
+	    prompt="Create \"~/$dotfile\" as symlink to \"$SCRIPT_DIR/$dotfile\"? (y/n/q)"
+	else
+	    prompt="Overwrite \"~/$dotfile\" with symlink to \"$SCRIPT_DIR/$dotfile\"? (y/n/q)"
+	fi
+	echo -n "$prompt "
+	read result
+    done
+
+    if [ "$result" = "q" ]; then
+	exit
+    fi
+    if [ "$result" = "n" ]; then
+	continue
+    fi
+
+    $LN -sf $SCRIPT_DIR/$dotfile $HOME_DIR/$dotfile
+done
+
