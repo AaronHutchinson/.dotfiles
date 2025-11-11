0e49a1c9be00393a05ff3658492a97185d8086d7
Aaron Hutchinson <hutchinsona2018@gmail.com>
2025-11-10 21:11:24 -0800

    Add smart-beginning-of-line and binding.

--- .emacs.d/keybindings.el
+++ .emacs.d/keybindings.el
@@ -11,3 +11,22 @@
 (global-set-key (kbd "C-c t") 'cmake-run-test)
 (global-set-key (kbd "C-c x") 'cmake-run-executable)
 (global-set-key (kbd "C-c a") 'cmake-run-all)
+
+(defun smart-beginning-of-line ()
+  "Move point to first non-whitespace character or beginning-of-line.
+
+Move point to the first non-whitespace character on the current line.
+If point was already at that position, move point to beginning of line."
+  (interactive)
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point))
+	 (beginning-of-line))))
+
+(defun smart-beginning-of-line-with-highlight ()
+  (interactive)
+  (unless mark-active (set-mark (point)))
+  (smart-beginning-of-line))
+
+(keymap-global-set "<home>" 'smart-beginning-of-line)
+(keymap-global-set "S-<home>" 'smart-beginning-of-line-with-highlight)
